656f03aafca6f980ea1f84d71635126172ab69ff
netlemon <netlemon2021@gmail.com>
2022-10-06 16:08:31 +0300

    Initial commit of modules repo

---
+++ services/webserver-cluster/outputs.tf
@@ -0,0 +1,16 @@
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
+
+output "asg_name" {
+  value       = aws_autoscaling_group.example.name
+  description = "The name of the ASG"
+}
+
+# This is exported in case for stage environment 
+# we want to open an extra port
+output "alb_security_group_id" {
+  value       = aws_security_group.alb.id
+  description = "The ID of the SG attached to the load balancer"
+}
---
+++ services/webserver-cluster/variables.tf
@@ -0,0 +1,44 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket used for the database's remote state storage"
+  type        = string
+}
+
+variable "db_remote_state_key" {
+  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance to run (e.g. t2.micro)"
+  type        = string
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 instances in the ASG"
+  type        = number
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 instances in the ASG"
+  type        = number
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
